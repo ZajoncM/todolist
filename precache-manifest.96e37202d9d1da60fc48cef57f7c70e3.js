f851df3e5ed233c26750cf12561ab8760ed7a7ea
Milosz Zajonc <milosz.zajonc@gmail.com>
2019-11-02 15:22:20 +0100

    Updates

---
+++ precache-manifest.96e37202d9d1da60fc48cef57f7c70e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d3d7beb660c027e497dec57e9f066f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "0a3a555aa09b6aa4d280",
+    "url": "/todolist/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "9879c7f5f794f7cc877d",
+    "url": "/todolist/static/css/main.f393011f.chunk.css"
+  },
+  {
+    "revision": "0a3a555aa09b6aa4d280",
+    "url": "/todolist/static/js/2.5c9bd739.chunk.js"
+  },
+  {
+    "revision": "9879c7f5f794f7cc877d",
+    "url": "/todolist/static/js/main.d3de6650.chunk.js"
+  },
+  {
+    "revision": "670214c0265e7a36915f",
+    "url": "/todolist/static/js/runtime-main.026291c4.js"
+  }
+]);
